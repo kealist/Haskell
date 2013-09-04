bae322a0ab8ca31b21583eb19e4d7e44f07bfd3b
kealist <kealist@gmail.com>
2013-09-04 08:38:27 -0500

    99 Problems: Problem 08

---
+++ 99 Problems/08.hs
@@ -0,0 +1,12 @@
+--(compress '(a a a a b c c a a d e e e e))
+--(A B C A D E)
+
+compress :: (Eq(a)) => [a] -> [a]
+compress [] = []
+compress x = compress' (init x) ((last x):[]) (last x) 
+
+compress' :: (Eq(a)) => [a] -> [a] -> a -> [a]
+compress' [] ys _ = ys
+compress' xs ys z = if last xs == z 
+							then (compress' (init xs) ys z) 
+							else (compress' (init xs) ((last xs):ys) (last xs))
