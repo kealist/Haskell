03b7c57455364ced5a55c8bbb63f00d70a37f678
kealist <kealist@gmail.com>
2013-09-10 18:18:01 -0500

    Update Problem 17 to not use predefined functions

--- 99 Problems/17.hs
+++ 99 Problems/17.hs
@@ -1,3 +1,10 @@
 
 split :: [a] -> Int -> ([a],[a])
-split x n = ((take n x), (drop n x)) 
+split x n = ((take' n x), (drop' n x))
+
+take' 0 _ = []
+take' 1 (x:xs) = [x]
+take' n (x:xs) = [x] ++ take' (n - 1) xs 
+
+drop' 0 x = x
+drop' n (x:xs) = drop' (n - 1) xs 
